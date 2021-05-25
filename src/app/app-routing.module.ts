d8edd5be2dab989321e27e94156b255047854f81
Gabe Ac <gabriel.ma.2288@gmail.com>
2021-05-25 13:17:51 -0700

    Step 4 - Adding Routes for the CRUD Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LearnerListComponent } from './components/learner-list/learner-list.component';
+import { LearnerDetailsComponent } from './components/learner-details/learner-details.component';
+import { LearnerCreateComponent } from './components/learner-create/learner-create.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'learners', pathMatch: 'full' },
+  { path: 'products', component: LearnerListComponent },
+  { path: 'products/:id', component: LearnerDetailsComponent },
+  { path: 'create', component: LearnerCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
